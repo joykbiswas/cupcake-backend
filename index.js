22335bfc2e712bb63c0bfed9c119a5bd6b8ac58b
joykbiswas <joykumarbiswas100@gmail.com>
2025-09-10 21:29:11 +0600

    deploy vercel

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 5000;
 //middleware
 app.use(cors(
   {
-    origin: ["http://localhost:5173",
+    origin: ["https://cupcake-two.vercel.app", "http://localhost:5173"
     ],
     Credential: true,
     optionSuccessStatus: 200,
